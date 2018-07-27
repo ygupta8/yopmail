191c06563e36506a101806b5018516fda7879648
ygupta8 <41792032+ygupta8@users.noreply.github.com>
2018-07-27 09:22:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-const YP = 'JAQL3ZQxlZGN0Zmt5AwpmAt';
-const YJ = 'IZGL4AQpmAGt0AGHkZwN3AD';
-const SPAM = 'false';
-const V = '2.7';
+const YP = 'HZGH4BGLkZmZ0ZwL1AQt0BQH';
+const YJ = 'AZwZlZmHjBGD5ZmLkZmDmZwp';
+const SPAM = 'true';
+const V = '2.8';
 
 const contains = (a, b) => {
     return a.toLowerCase().indexOf(b.toLowerCase()) >= 0;
 }
 
 const inbox = (id, phrase, p = 1) => {
-    return request.get(`http://m.yopmail.com/en/inbox.php?login=${id}&p=${p}&d=&ctrl=&scrl=&spam=${SPAM}&yf=005&yp=${YP}&yj=${YJ}&v=${V}&r_c=&id=`)
+    return request.get(`http://m.yopmail.com/en/inbox.php?login=${id}&p=${p}&d=&ctrl=&scrl=&spam=${SPAM}&yf=115&yp=${YP}&yj=${YJ}&v=${V}&r_c=&id=`)
         .then((result) => {
             const $ = cheerio.load(result);
             const mails = [];
